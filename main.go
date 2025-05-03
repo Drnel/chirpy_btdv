1a8a32868557a11c8cf260c8bb94e2701b61ab61
Drnel <mahesh121995@gmail.com>
2025-05-03 20:58:40 +0000

    added readyness handler

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ func main() {
 	var serve_mux = http.NewServeMux()
 	var server = http.Server{Handler: serve_mux}
 	server.Addr = ":8080"
-	serve_mux.Handle("/", http.FileServer(http.Dir(".")))
+
+	serve_mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	serve_mux.HandleFunc("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	}))
+
 	fmt.Println("Starting Chirpy server:")
 	server.ListenAndServe()
 }
